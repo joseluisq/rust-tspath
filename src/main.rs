8fdb506717f10d86593aa1f7a1c2e3551fc2cb1b
José Quintana <joseluisquintana20@gmail.com>
2019-02-24 00:44:46 +0100

    add read_dir and read_file functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+extern crate glob;
 extern crate log;
 extern crate simple_logger;
 
-use log::warn;
+use glob::glob;
+use log::{info, warn};
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::PathBuf;
 
 fn get_argument(args: &[String], key: &str, defaults: &str) -> String {
     let mut val = String::from("");
@@ -26,6 +32,35 @@ fn get_argument(args: &[String], key: &str, defaults: &str) -> String {
     }
 }
 
+fn read_file(path: &PathBuf) {
+    let display = path.display();
+
+    let buf_reader = match File::open(&path) {
+        Err(e) => panic!("couldn't open {}: {}", display, e),
+        Ok(file) => BufReader::new(file),
+    };
+
+    for (index, line) in buf_reader.lines().enumerate() {
+        info!("- FILE: {:?}", display);
+        info!("- LINE {}: {:?}", index, &line);
+
+        // TODO: Match path comparin first and replacing
+        match line {
+            Err(e) => warn!("{:?}", e),
+            Ok(line) => info!("- LINE {} (UP): {:?}", index, &line),
+        }
+    }
+}
+
+fn read_dir(pattern: &str) {
+    for entry in glob(pattern).expect("Failed to read glob pattern") {
+        match entry {
+            Err(e) => warn!("{:?}", e),
+            Ok(path) => read_file(&path),
+        }
+    }
+}
+
 fn main() {
     simple_logger::init().unwrap();
 
@@ -34,6 +69,10 @@ fn main() {
     let source = get_argument(&args, "--source", "./");
     let config = get_argument(&args, "--config", "tsconfig.json");
 
-    warn!("SOURCE: {}", source);
-    warn!("CONFIG: {}", config);
+    warn!("SOURCE: {}", &source);
+    warn!("CONFIG: {}", &config);
+
+    info!("--------");
+
+    read_dir(&source);
 }
